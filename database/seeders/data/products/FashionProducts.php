d4a3fa4f784a4dd4dda8b59fd96ed4b408728e72
Artt Vincii <otong123321@gmail.com>
2025-12-06 01:14:01 +0700

    Update Product Seeder

--- database/seeders/data/products/FashionProducts.php
+++ database/seeders/data/products/FashionProducts.php
@@ -1,200 +1,208 @@
 <?php
 
-return [
-    [
-        'title' => 'Batik Wanita',
-        'price' => 120000,
-        'description' => 'Batik wanita motif tradisional dengan bahan katun premium',
-        'url_media' => 'store/toko-elektronik-pramuka/products/batik-wanita.jpg',
-        'categories' => ['Fashion', 'Kecantikan']
-    ],
-    [
-        'title' => 'Batik Pria Modern',
-        'price' => 110000,
-        'description' => 'Batik pria dengan desain modern cocok untuk acara formal',
-        'url_media' => 'store/toko-elektronik-pramuka/products/batik.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Celana Panjang Formal',
-        'price' => 90000,
-        'description' => 'Celana panjang bahan kain berkualitas untuk kerja',
-        'url_media' => 'store/toko-elektronik-pramuka/products/celana.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jaket Universal',
-        'price' => 140000,
-        'description' => 'Jaket anti air cocok untuk segala cuaca',
-        'url_media' => 'store/toko-elektronik-pramuka/products/jaket.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jaket Pria Casual',
-        'price' => 110000,
-        'description' => 'Jaket casual untuk tampil santai namun tetap stylish',
-        'url_media' => 'store/toko-elektronik-pramuka/products/jaket2.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Kemeja Lengan Panjang',
-        'price' => 110000,
-        'description' => 'Kemeja formal lengan panjang berbagai warna',
-        'url_media' => 'store/toko-elektronik-pramuka/products/kemeja.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Dress Wanita Elegan',
-        'price' => 150000,
-        'description' => 'Dress wanita untuk acara formal dan pesta',
-        'url_media' => 'store/toko-elektronik-pramuka/products/dress.jpg',
-        'categories' => ['Fashion', 'Kecantikan']
-    ],
-    [
-        'title' => 'Kaos Polos Premium',
-        'price' => 50000,
-        'description' => 'Kaos polos cotton combed 30s berbagai warna',
-        'url_media' => 'store/toko-elektronik-pramuka/products/kaos.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Rok Midi Wanita',
-        'price' => 85000,
-        'description' => 'Rok midi dengan bahan jatuh dan nyaman',
-        'url_media' => 'store/toko-elektronik-pramuka/products/rok.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Blazer Formal Pria',
-        'price' => 200000,
-        'description' => 'Blazer formal untuk tampil profesional',
-        'url_media' => 'store/toko-elektronik-pramuka/products/blazer.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jeans Slim Fit',
-        'price' => 120000,
-        'description' => 'Celana jeans slim fit model terkini',
-        'url_media' => 'store/toko-elektronik-pramuka/products/jeans.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Sweater Rajut',
-        'price' => 95000,
-        'description' => 'Sweater rajut hangat untuk musim hujan',
-        'url_media' => 'store/toko-elektronik-pramuka/products/sweater.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Gamis Syari',
-        'price' => 180000,
-        'description' => 'Gamis syari dengan bahan jersey premium',
-        'url_media' => 'store/toko-elektronik-pramuka/products/gamis.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Hoodie Unisex',
-        'price' => 130000,
-        'description' => 'Hoodie nyaman untuk pria dan wanita',
-        'url_media' => 'store/toko-elektronik-pramuka/products/hoodie.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Kemeja Batik Kombinasi',
-        'price' => 125000,
-        'description' => 'Kemeja batik dengan kombinasi kain polos',
-        'url_media' => 'store/toko-elektronik-pramuka/products/kemeja-batik.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Celana Chino',
-        'price' => 95000,
-        'description' => 'Celana chino casual untuk berbagai acara',
-        'url_media' => 'store/toko-elektronik-pramuka/products/chino.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Tunik Wanita',
-        'price' => 100000,
-        'description' => 'Tunik wanita dengan motif cantik',
-        'url_media' => 'store/toko-elektronik-pramuka/products/tunik.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Polo Shirt',
-        'price' => 75000,
-        'description' => 'Polo shirt berkualitas untuk tampil casual',
-        'url_media' => 'store/toko-elektronik-pramuka/products/polo.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Cardigan Rajut',
-        'price' => 110000,
-        'description' => 'Cardigan rajut hangat dan stylish',
-        'url_media' => 'store/toko-elektronik-pramuka/products/cardigan.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jaket Jeans',
-        'price' => 145000,
-        'description' => 'Jaket jeans klasik yang tidak lekang waktu',
-        'url_media' => 'store/toko-elektronik-pramuka/products/jaket-jeans.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Blouse Wanita',
-        'price' => 90000,
-        'description' => 'Blouse wanita dengan desain elegan',
-        'url_media' => 'store/toko-elektronik-pramuka/products/blouse.jpg',
-        'categories' => ['Fashion', 'Kecantikan']
-    ],
-    [
-        'title' => 'Kemeja Flanel',
-        'price' => 85000,
-        'description' => 'Kemeja flanel motif kotak-kotak',
-        'url_media' => 'store/toko-elektronik-pramuka/products/flanel.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Kulot Pants',
-        'price' => 95000,
-        'description' => 'Celana kulot nyaman untuk wanita',
-        'url_media' => 'store/toko-elektronik-pramuka/products/kulot.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jaket Bomber',
-        'price' => 155000,
-        'description' => 'Jaket bomber dengan desain trendy',
-        'url_media' => 'store/toko-elektronik-pramuka/products/bomber.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Jumpsuit Wanita',
-        'price' => 165000,
-        'description' => 'Jumpsuit praktis dan modis',
-        'url_media' => 'store/toko-elektronik-pramuka/products/jumpsuit.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Outer Panjang',
-        'price' => 120000,
-        'description' => 'Outer panjang untuk layering',
-        'url_media' => 'store/toko-elektronik-pramuka/products/outer.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Celana Cargo',
-        'price' => 105000,
-        'description' => 'Celana cargo dengan banyak kantong',
-        'url_media' => 'store/toko-elektronik-pramuka/products/cargo.jpg',
-        'categories' => ['Fashion']
-    ],
-    [
-        'title' => 'Rompi Formal',
-        'price' => 130000,
-        'description' => 'Rompi formal untuk melengkapi setelan',
-        'url_media' => 'store/toko-elektronik-pramuka/products/rompi.jpg',
-        'categories' => ['Fashion']
-    ],
-];
+namespace Database\Seeders\Data\Products;
+
+class FashionProducts
+{
+    public static function getAll(): array
+    {
+        return [
+            [
+                'title' => 'Batik Wanita',
+                'price' => 120000,
+                'description' => 'Batik wanita motif tradisional dengan bahan katun premium',
+                'url_media' => 'store/toko-elektronik-pramuka/products/batik-wanita.jpg',
+                'categories' => ['Fashion', 'Kecantikan']
+            ],
+            [
+                'title' => 'Batik Pria Modern',
+                'price' => 110000,
+                'description' => 'Batik pria dengan desain modern cocok untuk acara formal',
+                'url_media' => 'store/toko-elektronik-pramuka/products/batik.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Celana Panjang Formal',
+                'price' => 90000,
+                'description' => 'Celana panjang bahan kain berkualitas untuk kerja',
+                'url_media' => 'store/toko-elektronik-pramuka/products/celana.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jaket Universal',
+                'price' => 140000,
+                'description' => 'Jaket anti air cocok untuk segala cuaca',
+                'url_media' => 'store/toko-elektronik-pramuka/products/jaket.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jaket Pria Casual',
+                'price' => 110000,
+                'description' => 'Jaket casual untuk tampil santai namun tetap stylish',
+                'url_media' => 'store/toko-elektronik-pramuka/products/jaket2.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Kemeja Lengan Panjang',
+                'price' => 110000,
+                'description' => 'Kemeja formal lengan panjang berbagai warna',
+                'url_media' => 'store/toko-elektronik-pramuka/products/kemeja.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Dress Wanita Elegan',
+                'price' => 150000,
+                'description' => 'Dress wanita untuk acara formal dan pesta',
+                'url_media' => 'store/toko-elektronik-pramuka/products/dress.jpg',
+                'categories' => ['Fashion', 'Kecantikan']
+            ],
+            [
+                'title' => 'Kaos Polos Premium',
+                'price' => 50000,
+                'description' => 'Kaos polos cotton combed 30s berbagai warna',
+                'url_media' => 'store/toko-elektronik-pramuka/products/kaos.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Rok Midi Wanita',
+                'price' => 85000,
+                'description' => 'Rok midi dengan bahan jatuh dan nyaman',
+                'url_media' => 'store/toko-elektronik-pramuka/products/rok.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Blazer Formal Pria',
+                'price' => 200000,
+                'description' => 'Blazer formal untuk tampil profesional',
+                'url_media' => 'store/toko-elektronik-pramuka/products/blazer.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jeans Slim Fit',
+                'price' => 120000,
+                'description' => 'Celana jeans slim fit model terkini',
+                'url_media' => 'store/toko-elektronik-pramuka/products/jeans.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Sweater Rajut',
+                'price' => 95000,
+                'description' => 'Sweater rajut hangat untuk musim hujan',
+                'url_media' => 'store/toko-elektronik-pramuka/products/sweater.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Gamis Syari',
+                'price' => 180000,
+                'description' => 'Gamis syari dengan bahan jersey premium',
+                'url_media' => 'store/toko-elektronik-pramuka/products/gamis.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Hoodie Unisex',
+                'price' => 130000,
+                'description' => 'Hoodie nyaman untuk pria dan wanita',
+                'url_media' => 'store/toko-elektronik-pramuka/products/hoodie.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Kemeja Batik Kombinasi',
+                'price' => 125000,
+                'description' => 'Kemeja batik dengan kombinasi kain polos',
+                'url_media' => 'store/toko-elektronik-pramuka/products/kemeja-batik.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Celana Chino',
+                'price' => 95000,
+                'description' => 'Celana chino casual untuk berbagai acara',
+                'url_media' => 'store/toko-elektronik-pramuka/products/chino.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Tunik Wanita',
+                'price' => 100000,
+                'description' => 'Tunik wanita dengan motif cantik',
+                'url_media' => 'store/toko-elektronik-pramuka/products/tunik.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Polo Shirt',
+                'price' => 75000,
+                'description' => 'Polo shirt berkualitas untuk tampil casual',
+                'url_media' => 'store/toko-elektronik-pramuka/products/polo.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Cardigan Rajut',
+                'price' => 110000,
+                'description' => 'Cardigan rajut hangat dan stylish',
+                'url_media' => 'store/toko-elektronik-pramuka/products/cardigan.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jaket Jeans',
+                'price' => 145000,
+                'description' => 'Jaket jeans klasik yang tidak lekang waktu',
+                'url_media' => 'store/toko-elektronik-pramuka/products/jaket-jeans.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Blouse Wanita',
+                'price' => 90000,
+                'description' => 'Blouse wanita dengan desain elegan',
+                'url_media' => 'store/toko-elektronik-pramuka/products/blouse.jpg',
+                'categories' => ['Fashion', 'Kecantikan']
+            ],
+            [
+                'title' => 'Kemeja Flanel',
+                'price' => 85000,
+                'description' => 'Kemeja flanel motif kotak-kotak',
+                'url_media' => 'store/toko-elektronik-pramuka/products/flanel.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Kulot Pants',
+                'price' => 95000,
+                'description' => 'Celana kulot nyaman untuk wanita',
+                'url_media' => 'store/toko-elektronik-pramuka/products/kulot.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jaket Bomber',
+                'price' => 155000,
+                'description' => 'Jaket bomber dengan desain trendy',
+                'url_media' => 'store/toko-elektronik-pramuka/products/bomber.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Jumpsuit Wanita',
+                'price' => 165000,
+                'description' => 'Jumpsuit praktis dan modis',
+                'url_media' => 'store/toko-elektronik-pramuka/products/jumpsuit.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Outer Panjang',
+                'price' => 120000,
+                'description' => 'Outer panjang untuk layering',
+                'url_media' => 'store/toko-elektronik-pramuka/products/outer.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Celana Cargo',
+                'price' => 105000,
+                'description' => 'Celana cargo dengan banyak kantong',
+                'url_media' => 'store/toko-elektronik-pramuka/products/cargo.jpg',
+                'categories' => ['Fashion']
+            ],
+            [
+                'title' => 'Rompi Formal',
+                'price' => 130000,
+                'description' => 'Rompi formal untuk melengkapi setelan',
+                'url_media' => 'store/toko-elektronik-pramuka/products/rompi.jpg',
+                'categories' => ['Fashion']
+            ],
+        ];
+    }
+}
--- database/seeders/data/products/SouvenirProducts.php
+++ database/seeders/data/products/SouvenirProducts.php
@@ -1,46 +1,54 @@
 <?php
 
-return [
-    [
-        'title' => 'Souvenir Pernikahan Kotak Kecil',
-        'price' => 15000,
-        'description' => 'Kotak kecil cantik untuk souvenir pernikahan dengan pita dekoratif.',
-        'url_media' => 'products/souvenir/souvenir-kotak.jpg',
-        'categories' => ['Souvenir'],
-    ],
-    [
-        'title' => 'Mug Custom Printing',
-        'price' => 35000,
-        'description' => 'Mug putih dengan cetak gambar/logo sesuai pesanan.',
-        'url_media' => 'products/souvenir/mug-custom.jpg',
-        'categories' => ['Souvenir'],
-    ],
-    [
-        'title' => 'Tas Kanvas Mini',
-        'price' => 25000,
-        'description' => 'Tas kanvas kecil cocok untuk souvenir seminar atau event kecil.',
-        'url_media' => 'products/souvenir/tas-kanvas.jpg',
-        'categories' => ['Souvenir'],
-    ],
-    [
-        'title' => 'Notebook Mini',
-        'price' => 15000,
-        'description' => 'Notebook mini dengan cover custom, cocok untuk seminar atau event.',
-        'url_media' => 'products/souvenir/notebook.jpg',
-        'categories' => ['Souvenir'],
-    ],
-    [
-        'title' => 'Tempat Pensil Kayu',
-        'price' => 20000,
-        'description' => 'Tempat pensil dari kayu dengan ukiran nama atau logo custom.',
-        'url_media' => 'products/souvenir/tempat-pensil.jpg',
-        'categories' => ['Souvenir'],
-    ],
-    [
-        'title' => 'Mini Plant Pot',
-        'price' => 25000,
-        'description' => 'Pot tanaman mini untuk souvenir, bisa custom tulisan atau logo.',
-        'url_media' => 'products/souvenir/mini-pot.jpg',
-        'categories' => ['Souvenir'],
-    ],
-];
+namespace Database\Seeders\Data\Products;
+
+class SouvenirProducts
+{
+    public static function getAll(): array
+    {
+        return [
+            [
+                'title' => 'Souvenir Pernikahan Kotak Kecil',
+                'price' => 15000,
+                'description' => 'Kotak kecil cantik untuk souvenir pernikahan dengan pita dekoratif.',
+                'url_media' => 'products/souvenir/souvenir-kotak.jpg',
+                'categories' => ['Souvenir'],
+            ],
+            [
+                'title' => 'Mug Custom Printing',
+                'price' => 35000,
+                'description' => 'Mug putih dengan cetak gambar/logo sesuai pesanan.',
+                'url_media' => 'products/souvenir/mug-custom.jpg',
+                'categories' => ['Souvenir'],
+            ],
+            [
+                'title' => 'Tas Kanvas Mini',
+                'price' => 25000,
+                'description' => 'Tas kanvas kecil cocok untuk souvenir seminar atau event kecil.',
+                'url_media' => 'products/souvenir/tas-kanvas.jpg',
+                'categories' => ['Souvenir'],
+            ],
+            [
+                'title' => 'Notebook Mini',
+                'price' => 15000,
+                'description' => 'Notebook mini dengan cover custom, cocok untuk seminar atau event.',
+                'url_media' => 'products/souvenir/notebook.jpg',
+                'categories' => ['Souvenir'],
+            ],
+            [
+                'title' => 'Tempat Pensil Kayu',
+                'price' => 20000,
+                'description' => 'Tempat pensil dari kayu dengan ukiran nama atau logo custom.',
+                'url_media' => 'products/souvenir/tempat-pensil.jpg',
+                'categories' => ['Souvenir'],
+            ],
+            [
+                'title' => 'Mini Plant Pot',
+                'price' => 25000,
+                'description' => 'Pot tanaman mini untuk souvenir, bisa custom tulisan atau logo.',
+                'url_media' => 'products/souvenir/mini-pot.jpg',
+                'categories' => ['Souvenir'],
+            ],
+        ];
+    }
+}
