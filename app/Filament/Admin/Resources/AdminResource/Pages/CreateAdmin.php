7cb9ced617bd3e8c18c2022aeef067d69b775577
Alfandy Afriandani <alfandy.afriandani@gmail.com>
2025-12-07 17:45:14 +0700

    fix: Urgentt coegg udah habis waktu jadi gini aja hehe admin langsung kebuat superadmin aja

--- app/Filament/Admin/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Admin/Resources/AdminResource/Pages/CreateAdmin.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAdmin extends CreateRecord
 {
     protected static string $resource = AdminResource::class;
+
+    protected function afterCreate(): void
+    {
+
+        $this->record->assignRole('super_admin');
+    }
 }
